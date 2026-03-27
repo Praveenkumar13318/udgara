f6ed4c017a41de3f16e9a5fcc095595393e561b1
Praveen <your-email@gmail.com>
2026-03-27 11:10:20 +0530

    add fevicon

---
+++ app/head.tsx
@@ -0,0 +1,14 @@
+export default function Head() {
+  return (
+    <>
+      <title>Udgara</title>
+      <meta name="description" content="Voice platform" />
+
+      {/* 🔥 FAVICON */}
+      <link rel="icon" href="/favicon.ico" />
+
+      {/* OPTIONAL */}
+      <meta name="theme-color" content="#111111" />
+    </>
+  );
+}
