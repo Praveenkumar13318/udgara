ce44b1973dd6bf6e56a3c87c1759dcfefe8c5f0a
Praveen <your-email@gmail.com>
2026-04-08 15:00:44 +0530

    scrool fix final jump

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,22 +1,25 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import { useRef } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() =>
-    new QueryClient({
+
+  const queryClientRef = useRef<QueryClient | null>(null);
+
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient({
       defaultOptions: {
         queries: {
-          staleTime: 1000 * 60 * 5, // 5 min cache
+          staleTime: 1000 * 60 * 5,
           refetchOnWindowFocus: false,
         },
       },
-    })
-  );
+    });
+  }
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClientRef.current}>
       {children}
     </QueryClientProvider>
   );
