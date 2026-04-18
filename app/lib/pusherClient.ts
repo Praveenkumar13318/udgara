0e576b6e4c4d241032733d31936cda89c3d849e8
Praveen <your-email@gmail.com>
2026-04-18 13:16:46 +0530

    cloud error fix1

--- app/lib/pusherClient.ts
+++ app/lib/pusherClient.ts
@@ -1,10 +1,18 @@
 import PusherClient from "pusher-js";
 
 let pusherClient: PusherClient | null = null;
+
 if (typeof window !== "undefined") {
-  pusherClient = new PusherClient(
-    process.env.NEXT_PUBLIC_PUSHER_KEY!,
-    { cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER ?? "ap2" }
-  );
+  const key = process.env.NEXT_PUBLIC_PUSHER_KEY;
+  const cluster = process.env.NEXT_PUBLIC_PUSHER_CLUSTER;
+
+  if (!key) {
+    console.error("❌ PUSHER KEY MISSING");
+  } else {
+    pusherClient = new PusherClient(key, {
+      cluster: cluster || "ap2",
+    });
+  }
 }
+
 export { pusherClient };
